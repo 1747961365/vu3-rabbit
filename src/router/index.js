b482a167d631b6dd77858b7fe7b14b3a688c0715
Toyokra <1747961365@qq.com>
2023-05-24 21:01:28 +0800

    二级分类和路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/home/index.vue'
 import Category from '@/views/category/index.vue'
+import SubCategory from '@/views/SubCategory/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,6 +20,10 @@ const router = createRouter({
           path: 'category/:id',
           name: 'category',
           component: Category,
+        },
+        {
+          path: 'category/sub/:id',
+          component: SubCategory,
         },
       ],
     },
