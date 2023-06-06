27af633fea542fc209318458fa8e5a499d3da0b4
Toyokra <1747961365@qq.com>
2023-06-06 14:47:33 +0800

    pinia用户数据持久化

--- src/main.js
+++ src/main.js
@@ -2,14 +2,16 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 //引入初始化样式
 import '@/styles/common.scss'
 import { lazyPlugin } from '@/directives'
 import {componentsPlugin} from '@/components/index'
 const app = createApp(App)
-
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
 app.use(componentsPlugin)
