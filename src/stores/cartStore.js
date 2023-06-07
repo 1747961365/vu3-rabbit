513d76abeed00d9b2ff95db3d00f22010e762096
Toyokra <1747961365@qq.com>
2023-06-07 21:34:34 +0800

    头部购物车删除

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -14,9 +14,14 @@ export const useCartStroe = defineStore('cart', () => {
             }
         }
     }
+    const delCart =(skuId) =>{
+        const idx = cartList.value.findIndex((item) => skuId === item.skuId)
+        cartList.value.splice(idx, 1)
+    }
     return {
         cartList,
-        addCart
+        addCart,
+        delCart
     }
 },{
     persist: true,
